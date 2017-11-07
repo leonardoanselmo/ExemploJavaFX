0a3932205cd8c88fb381be965f7e39d40a76a6bb
Leonardo Anselmo <leonardo_anselmo@hotmail.com>
2017-11-07 20:48:28 -0300

    Classe Main principal

--- src/controller/Main.java
+++ src/controller/Main.java
@@ -8,12 +8,40 @@ import javafx.stage.Stage;
 
 public class Main extends Application {
 
+    private static Stage stage;
+
+    private static Scene mainScene;
+    private static Scene detailsScene;
+
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("../view/main_screen.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+
+        stage = primaryStage;
+
+        primaryStage.setTitle("Exemplo JavaFX 8");
+
+        Parent fxmlMain = FXMLLoader.load(getClass().getResource("../view/main_screen.fxml"));
+        mainScene = new Scene(fxmlMain, 640, 400);
+
+        Parent fxmlDetails = FXMLLoader.load(getClass().getResource("../view/details_screen.fxml"));
+        detailsScene = new Scene(fxmlDetails, 640, 400);
+
+        primaryStage.setScene(mainScene);
         primaryStage.show();
+    }
+
+    public static void changeScreen(String scr){
+
+        switch (scr){
+            case "main":
+                stage.setScene(mainScene);
+                break;
+            case "details":
+                stage.setScene(detailsScene);
+                break;
+
+        }
+
     }
 
 
