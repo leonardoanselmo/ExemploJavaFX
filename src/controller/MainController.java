f51d0dc155676fec881bc318d43adae885b43b6f
Leonardo Anselmo <leonardo_anselmo@hotmail.com>
2017-11-07 20:49:09 -0300

    Classe main controller

--- src/controller/MainController.java
+++ src/controller/MainController.java
@@ -1,4 +1,18 @@
 package controller;
 
-public class Controller {
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+public class MainController {
+
+    @FXML
+    protected void btNovoAction(ActionEvent e){
+        System.out.println("bt Novo!");
+
+        Main.changeScreen("details");
+    }
+
+
+
+
 }
